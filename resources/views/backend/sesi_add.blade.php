276981dc9fa49a2602014169f211fba57f6e5fe7
Amy <mimishasha13@gmail.com>
2018-07-25 16:51:50 +0800

    create table sesi

---
+++ resources/views/backend/sesi_add.blade.php
@@ -0,0 +1,48 @@
+@extends('backend.layout')
+
+@section('content')
+
+    <!-- Page Content -->
+    <section class="py-5">
+      <div class="container">
+        <h1>Tambah Sesi</h1><br/>
+
+        @include('common.alert')
+        @include('common.form_error')
+
+        <form method="POST" action="{{ route('user.register.post') }}">
+          @csrf
+          <div class="form-group row">
+            <label for="inputPassword3" class="col-sm-2 col-form-label">Name</label>
+            <div class="col-sm-10">
+              <input name="name" type="text" class="form-control" id="name" placeholder="Name" value="{{ old('name') }}">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+            <div class="col-sm-10">
+              <input name="email" type="email" class="form-control" id="inputEmail3" placeholder="Email" value="{{ old('email') }}">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputPassword3" class="col-sm-2 col-form-label">IC No</label>
+            <div class="col-sm-10">
+              <input name="icno" type="text" class="form-control" id="icno" placeholder="IC No without dash (-)" value="{{ old('icno') }}">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+            <div class="col-sm-10">
+              <input name="password" type="password" class="form-control" id="inputPassword3" placeholder="Password">
+            </div>
+          </div>
+          <div class="form-group row">
+            <div class="col-sm-10">
+              <button type="submit" class="btn btn-primary">Register</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+
+@endsection
